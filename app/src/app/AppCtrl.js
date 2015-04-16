e389f3146a051d28e11678af64280a5489d1e307
pgu <guilcher.pascal@gmail.com>
2015-04-16 15:28:16 +0200

    feat(contacts): crud app in standalone mode

---
+++ app/src/app/AppCtrl.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('starterApp')
+  .controller('AppCtrl', function ($scope, $mdToast, //
+                                   $q, NotificationsService) {
+
+    function showMessage (type, toastPosition) {
+      return function (message) {
+
+        if (_.isEmpty(message)) {
+          return $q.when();
+
+        } else {
+
+          return $mdToast.show(
+            $mdToast.simple()
+              .content(message)
+              .position(toastPosition)
+          );
+
+        }
+      }
+    }
+
+    var showError = showMessage('error', 'top left');
+    var showInfo = showMessage('info', 'top right');
+
+    $scope.$watch(function () {
+      return NotificationsService.notifications;
+    }, function (notifications) {
+
+      showError(notifications.error)
+        .finally(function () {
+          return showInfo(notifications.info);
+        })
+        .finally(function () {
+          NotificationsService.clear();
+        })
+      ;
+
+    }, true);
+
+  });
+
